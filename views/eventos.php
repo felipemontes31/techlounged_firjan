643fce3891d978598c66a36f8f8872b4ffb3cacd
Charles <charles93222@yahoo.com.br>
2026-05-28 15:41:21 -0300

    test

--- views/eventos.php
+++ views/eventos.php
@@ -1,5 +1,5 @@
-<?php require_once(__DIR__ . '/../config/app.php'); if (session_status() === PHP_SESSION_NONE) session_start(); 
-
+<?php 
+require_once(__DIR__ . '/../config/app.php'); if (session_status() === PHP_SESSION_NONE) session_start(); 
 ?>
 
 <!DOCTYPE html>
